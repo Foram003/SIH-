2dc30a2ce83766cd6901e9d57e45af7bc7b022d0
FORAM <foramgandhi003@gmail.com>
2023-10-30 20:00:42 +0530

    updated ministry

--- lib/register.dart
+++ lib/register.dart
@@ -8,7 +8,7 @@ class MyRegister extends StatefulWidget {
 }
 
 class _MyRegisterState extends State<MyRegister> {
-  String dropdownValue = 'Option 1'; // Initial dropdown value
+  String dropdownValue = 'All Ministry'; // Initial dropdown value
 
   double fieldWidth = 300.0; // Set the width of text fields and dropdown
   double fieldHeight = 60.0; // Set the height of text fields and dropdown
@@ -58,7 +58,7 @@ class _MyRegisterState extends State<MyRegister> {
                   children: [
                     Container(
                       width: fieldWidth,
-                      height: fieldHeight, // Set the height
+                      height: fieldHeight,
                       child: TextField(
                         decoration: InputDecoration(
                           focusedBorder: inputBorder,
@@ -68,12 +68,10 @@ class _MyRegisterState extends State<MyRegister> {
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: 30,
-                    ),
+                    SizedBox(height: 30),
                     Container(
                       width: fieldWidth,
-                      height: fieldHeight, // Set the height
+                      height: fieldHeight,
                       child: TextField(
                         decoration: InputDecoration(
                           focusedBorder: inputBorder,
@@ -83,12 +81,10 @@ class _MyRegisterState extends State<MyRegister> {
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: 30,
-                    ),
+                    SizedBox(height: 30),
                     Container(
                       width: fieldWidth,
-                      height: fieldHeight, // Set the height
+                      height: fieldHeight,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
                         border: Border.all(color: Colors.black),
@@ -109,7 +105,97 @@ class _MyRegisterState extends State<MyRegister> {
                             dropdownValue = newValue!;
                           });
                         },
-                        items: <String>['Option 1', 'Option 2', 'Option 3', 'Option 4']
+                        items: <String>[
+                          'All Ministry',
+                          "President's Secretariat",
+                          "Vice President's Secretariat",
+                          "Prime Minister's Office",
+                          'Cabinet',
+                          'Cabinet Committee Decisions',
+                          'Cabinet Committee on Economic Affairs (CCEA)',
+                          'Cabinet Secretariat',
+                          'Cabinet Committee on Infrastructure',
+                          'Cabinet Committee on Price',
+                          'Cabinet Committee on Investment',
+                          'AYUSH',
+                          'Other Cabinet Committees',
+                          'Department of Space',
+                          'Department of Ocean Development',
+                          'Department of Atomic Energy',
+                          'Election Commission',
+                          'Finance Commission',
+                          'Ministry of Agriculture & Farmers Welfare',
+                          'Ministry of Agro & Rural Industries',
+                          'Ministry of Chemicals and Fertilizers',
+                          'Ministry of Civil Aviation',
+                          'Ministry of Coal',
+                          'Ministry of Commerce & Industry',
+                          'Ministry of Communications',
+                          'Ministry of Company Affairs',
+                          'Ministry of Consumer Affairs, Food & Public Distribution',
+                          'Ministry of Cooperation',
+                          'Ministry of Corporate Affairs',
+                          'Ministry of Culture',
+                          'Ministry of Defence',
+                          'Ministry of Development of North-East Region',
+                          'Ministry of Disinvestment',
+                          'Ministry of Drinking Water & Sanitation',
+                          'Ministry of Earth Sciences',
+                          'Ministry of Education',
+                          'Ministry of Electronics & IT',
+                          'Ministry of Environment, Forest and Climate Change',
+                          'Ministry of External Affairs',
+                          'Ministry of Finance',
+                          'Ministry of Fisheries, Animal Husbandry & Dairying',
+                          'Ministry of Food Processing Industries',
+                          'Ministry of Health and Family Welfare',
+                          'Ministry of Heavy Industries',
+                          'Ministry of Home Affairs',
+                          'Ministry of Housing & Urban Affairs',
+                          'Ministry of Information & Broadcasting',
+                          'Ministry of Jal Shakti',
+                          'Ministry of Labour & Employment',
+                          'Ministry of Law and Justice',
+                          'Ministry of Micro, Small & Medium Enterprises',
+                          'Ministry of Mines',
+                          'Ministry of Minority Affairs',
+                          'Ministry of New and Renewable Energy',
+                          'Ministry of Overseas Indian Affairs',
+                          'Ministry of Panchayati Raj',
+                          'Ministry of Parliamentary Affairs',
+                          'Ministry of Personnel, Public Grievances & Pensions',
+                          'Ministry of Petroleum & Natural Gas',
+                          'Ministry of Planning',
+                          'Ministry of Power',
+                          'Ministry of Railways',
+                          'Ministry of Road Transport & Highways',
+                          'Ministry of Rural Development',
+                          'Ministry of Science & Technology',
+                          'Ministry of Ports, Shipping and Waterways',
+                          'Ministry of Skill Development and Entrepreneurship',
+                          'Ministry of Social Justice & Empowerment',
+                          'Ministry of Statistics & Programme Implementation',
+                          'Ministry of Steel',
+                          'Ministry of Surface Transport',
+                          'Ministry of Textiles',
+                          'Ministry of Tourism',
+                          'Ministry of Tribal Affairs',
+                          'Ministry of Urban Development',
+                          'Ministry of Water Resources, River Development and Ganga Rejuvenation',
+                          'Ministry of Women and Child Development',
+                          'Ministry of Youth Affairs and Sports',
+                          'NITI Aayog',
+                          'PM Speech',
+                          'EAC-PM',
+                          'UPSC',
+                          'Special Service and Features',
+                          'PIB Headquarters',
+                          'Office of Principal Scientific Advisor to GoI',
+                          'National Financial Reporting Authority',
+                          'Competition Commission of India',
+                          'IFSC Authority',
+                          'National Security Council Secretariat',
+                        ]
                             .map<DropdownMenuItem<String>>((String value) {
                           return DropdownMenuItem<String>(
                             value: value,
@@ -124,12 +210,10 @@ class _MyRegisterState extends State<MyRegister> {
                         }).toList(),
                       ),
                     ),
-                    SizedBox(
-                      height: 30,
-                    ),
+                    SizedBox(height: 30),
                     Container(
                       width: fieldWidth,
-                      height: fieldHeight, // Set the height
+                      height: fieldHeight,
                       child: TextField(
                         obscureText: true,
                         decoration: InputDecoration(
@@ -188,11 +272,11 @@ class _MyRegisterState extends State<MyRegister> {
                           ),
                         ),
                       ],
-                    )
+                    ),
                   ],
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
